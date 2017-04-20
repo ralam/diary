f9787da1dff6d1857869fcde1b1b0ca8a4693cc9
ralam <rehan.alam@gmail.com>
2017-04-20 19:15:34 -0400

    reworked format date function to account for timezone offset

--- frontend/util/date_util.js
+++ frontend/util/date_util.js
@@ -5,4 +5,19 @@ export const formatToHumanDate = dateObj => {
     return `${daysMap[dateObj.getDay()]}, ${monthsMap[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`
 }
 
-export const formatDate = dateObj => dateObj.toISOString().slice(0, 10);
+export const formatDate = dateObj => {
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth() + 1;
+    const year = dateObj.getFullYear();
+    let dateString = `${year}-`;
+    if(month < 10) {
+        dateString += '0';
+    }
+    dateString += `${month}-`;
+    if(day < 10) {
+        dateString += '0';
+    }
+    dateString += `${day}`;
+
+    return dateString;
+}
